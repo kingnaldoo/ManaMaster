59c5ca3ce8e23250882b4bcf0ce160376852aff3
kingnaldoo <reinaldosn2016@gmail.com>
2023-01-13 10:28:29 -0300

    feat: android launch icon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import {
-  TransitionPresets,
-  createStackNavigator,
-} from "@react-navigation/stack";
 import { PrivateRoutes } from "./private.routes";
 import { PublicRoutes } from "./public.routes";
 
 export function Routes() {
-  const Stack = createStackNavigator();
   const isAuth = false;
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          ...TransitionPresets.SlideFromRightIOS,
-        }}
-      >
-        {isAuth ? <PrivateRoutes /> : <PublicRoutes />}
-      </Stack.Navigator>
+      {isAuth ? <PrivateRoutes /> : <PublicRoutes />}
     </NavigationContainer>
   );
 }
