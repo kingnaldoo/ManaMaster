5712d1290dbd25b4175acae830ce8a973df88775
kingnaldoo <reinaldosn2016@gmail.com>
2023-01-13 11:00:43 -0300

    feat: add android splashscreen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect } from "react";
+import { Platform, StatusBar } from "react-native";
+import SplashScreen from "react-native-splash-screen";
 import { Routes } from "./src/routes";
 
 export default function App() {
+  useEffect(() => {
+    StatusBar.setBarStyle("light-content");
+
+    if (Platform.OS === "android") {
+      StatusBar.setBackgroundColor("transparent");
+      StatusBar.setTranslucent(false);
+    }
+
+    SplashScreen.hide();
+  }, []);
+
   return <Routes />;
 }
